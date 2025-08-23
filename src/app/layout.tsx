a64b0099c2139c0eaf132d66a02d1edc32d8b753
jiwon <leeji.circleu@gmail.com>
2025-08-23 10:52:42 +0900

    수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,25 +16,25 @@ export const metadata: Metadata = {
 
 import localFont from 'next/font/local';
 
-const pretendard = localFont({
+export const pretendard = localFont({
   src: [
     {
-      path: '../../public/fonts/Pretendard/Pretendard-Regular.woff2',
+      path: '/fonts/pretendard/Pretendard-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/pretendard/Pretendard-Medium.woff2',
+      path: '/fonts/pretendard/Pretendard-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../public/pretendard/Pretendard-SemiBold.woff2',
+      path: '/fonts/pretendard/Pretendard-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../../public/pretendard/Pretendard-Bold.woff2',
+      path: '/fonts/pretendard/Pretendard-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
